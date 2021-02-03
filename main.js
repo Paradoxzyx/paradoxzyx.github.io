1bee91c9bda125954ee4e99ccabe06a0e4d376ac
Paradoxzyx <48637000+Paradoxzyx@users.noreply.github.com>
2021-02-03 16:46:24 +1300

    Update main.js

--- main.js
+++ main.js
@@ -173,13 +173,11 @@ function add(id) {
   })
   
   //--- Update URL
-  if (id != 0) {
-    url[active].push(id)
-    url[active].sort(function(a, b) {
-      return a - b
-    })
-    history.replaceState(null, "", "?c=" + active + "&s=" + url[active].join(","))
-  }
+  url[active].push(id)
+  url[active].sort(function(a, b) {
+    return a - b
+  })
+  history.replaceState(null, "", "?c=" + active + "&s=" + url[active].join(","))
 }
 
 //---------------------------------------- Remove node
