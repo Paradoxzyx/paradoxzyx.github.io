66ca7cd2bc2f392ea757b86bebc6550834d488c0
Paradoxzyx <48637000+Paradoxzyx@users.noreply.github.com>
2021-03-02 00:01:07 +1300

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ $(() => {
   let keywords = {
     "hl-a": /(anomaly power)/gi,
     "hl-d": /((weapon|assault|close range|long range) damage|firepower)/gi,
-    "hl-e": /(weakness|mark(ed)?|burn(ing)?|ash(ed)?|bleed|toxic|vulnerab(le|ility)|freeze|frozen)/gi,
+    "hl-e": /(weakness|vulnerab(le|ility)|marked|burn(ing)?|ash(ed)?|bleed|toxic|freeze|frozen)/gi,
     "hl-h": /((maximum )?health( regen)?)/gi,
     "hl-l": /(^\[hm\]|(weapon|skill) leech|heal(?!th)(s|ed|ing)?)/gi,
     "hl-n": /((concentration|magma golem|anomaly in veins|br\/8 impact amplifier) node)/gi,
